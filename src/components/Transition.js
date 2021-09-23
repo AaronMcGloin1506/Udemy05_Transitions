219f0b38fd6bd406a72a840ad6d9d7c781cef4e6
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-09-23 11:38:21 +0100

    part 2 transitions

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -18,16 +18,25 @@ class TransitionComp extends Component{
             <>
                 <Transition
                     in={this.state.show}
-                    timeout={2000}
+                    timeout={{
+                        enter:2000,
+                        exit:50
+                    }}
+
+                    // enter={false} //disabling the entering state
+                    // exit={false} // disabling the exiting state
+
+                    onEnter={() =>{
+                        console.log('ENTER')
+                    }}
+
+
+                    onExit={() =>{
+                        console.log('EXIT')
+                    }}
                 >
                     { state => (
-                        <div 
-                            style={{
-                                    background:'red',
-                                    height:'100px',
-                                    transition:'all 2s ease',
-                                    opacity: state === 'exited' || state === 'exiting' ? 0 : 1 
-                            }}>{state}</div> 
+                        <div className={`square square-${state}`}>{`square square-${state}`}</div> 
                     ) }
                 </Transition>
 
